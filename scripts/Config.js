51fad1777569f6f942e352775d03e9b4777d3a2f
Schen <schen@jerseymikes.com>
2019-06-05 14:03:51 -0400

    test

--- scripts/Config.js
+++ scripts/Config.js
@@ -2,7 +2,7 @@ Config = {
 	MAINTENANCE_MODE: false,
 	CHAIN_ID: 'ssc-mainnet1',
 	RPC_URL: 'https://api.steem-engine.com/rpc',
-	ACCOUNTS_API_URL: 'https://anyx.io',
+	ACCOUNTS_API_URL: 'https://api.steem-engine.com/accounts',
 	CONVERTER_API: 'https://converter-api.steem-engine.com/api',
 	SCOT_API: 'https://scot-api.steem-engine.com/',
 	NATIVE_TOKEN: 'ENG',
